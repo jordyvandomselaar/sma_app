d27f88476f7855d131d327852e9d70a1dbd15753
Jordy van Domselaar <jordy@deinternetjongens.nl>
2019-01-09 17:28:50 +0100

    Show if message is valid with bar underneath card

--- lib/MessageCard.dart
+++ lib/MessageCard.dart
@@ -9,28 +9,36 @@ class MessageCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(children: <Widget>[
-            Padding(
-                child: Text("Je bericht",
-                    style:
+      child: Column(
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(children: <Widget>[
+                Padding(
+                    child: Text("Je bericht",
+                        style:
                         TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
-                padding: EdgeInsets.only(top: 10, bottom: 10)),
-            Text(message.message),
-            Padding(
-                child: Text("Url",
-                    style:
+                    padding: EdgeInsets.only(top: 10, bottom: 10)),
+                Text(message.message),
+                Padding(
+                    child: Text("Url",
+                        style:
                         TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
-                padding: EdgeInsets.only(top: 10, bottom: 10)),
-            Text(message.url),
-            Padding(
-                child: Text("Wachtwoord",
-                    style:
+                    padding: EdgeInsets.only(top: 10, bottom: 10)),
+                Text(message.url),
+                Padding(
+                    child: Text("Wachtwoord",
+                        style:
                         TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
-                padding: EdgeInsets.only(top: 10, bottom: 10)),
-            Text(message.password),
-          ])),
+                    padding: EdgeInsets.only(top: 10, bottom: 10)),
+                Text(message.password),
+              ])),
+          Container(
+            height: 5,
+            color: this.message.isValid() ? Colors.blueAccent : Colors.redAccent,
+          )
+        ],
+      ),
     );
   }
 }
